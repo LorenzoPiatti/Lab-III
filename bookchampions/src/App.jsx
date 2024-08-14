fc3485bd7e8a3047e669b28facc84fc296c26952
Piatti, Lorenzo <lorenzopiatti2003@gmail.com>
2024-08-14 19:32:39 -0300

    Initial commit

---
+++ bookchampions/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h2>Books Champion App</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
